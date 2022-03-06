ac50590e5ed627d767407fe42bb122332c588623
OstapenkoK <fodnichanel@gmail.com>
2022-03-06 22:30:18 +0300

    Working shop sorting

--- assets/js/shop-categories.js
+++ assets/js/shop-categories.js
@@ -3,4 +3,22 @@ var categoriesList = $('.shop-categories');
 function categChange(category) {
 	categoriesList.children().removeClass('shop-categ-active');
 	category.parentNode.classList.add('shop-categ-active');
+
+	switch(category.name) {
+		case "feautured":
+			$('.featured-products').css('display', 'block');
+			$('.new-products').css('display', 'none');
+			$('.best-seller').css('display', 'none');
+			break;
+		case "new":
+			$('.featured-products').css('display', 'none');
+			$('.new-products').css('display', 'block');
+			$('.best-seller').css('display', 'none');
+			break;
+		case "best":
+			$('.featured-products').css('display', 'none');
+			$('.new-products').css('display', 'none');
+			$('.best-seller').css('display', 'block');
+			break;
+	}
 }
