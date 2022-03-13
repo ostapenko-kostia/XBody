f389688d4ea4f4092a6e1c4df9a97b764dd2ea67
OstapenkoK <fodnichanel@gmail.com>
2022-03-13 20:39:55 +0300

    Added modal window of menu on 425px or lower

--- assets/js/shop--sidebar.js
+++ assets/js/shop--sidebar.js
@@ -4,14 +4,21 @@ var shop = $('.topShop');
 
 var clicked = false;
 
+const width = window.innerWidth;
+
 btn.on("click", function() {
-	if (!clicked) {
-		clicked = true;
-		shop.css("display", 'none');
-		sidebar.css("display", 'block');
-	} else if (clicked) {
-		clicked = false;
-		sidebar.css("display", 'none');
-		shop.css("display", 'block');
+	if (width <= 768 && width > 425) {
+		if (!clicked) {
+			clicked = true;
+			shop.css("display", 'none');
+			sidebar.css("display", 'block');
+		} else if (clicked) {
+			clicked = false;
+			sidebar.css("display", 'none');
+			shop.css("display", 'block');
+		}
+	}
+	if (width <= 425) {
+		sidebar.toggleClass('shop-active');
 	}
 });
